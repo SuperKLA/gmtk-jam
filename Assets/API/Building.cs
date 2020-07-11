7dccbc8d6ae7ba08ff3d71513ecaccc383a1b9e9
Konstantin Lapin <xKosta.1987@gmail.com>
2020-07-11 13:28:12 +0200

    Masken besser

--- Assets/API/Building.cs
+++ Assets/API/Building.cs
@@ -1,8 +1,30 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteAlways]
 public class Building : MonoBehaviour
 {
+    public SpriteRenderer Renderer;
+    public SpriteMask MaskRenderer;
+    public Transform MaskUp;
+    public Transform MaskFront;
     
+    private void Update()
+    {
+        var sortingFront = Mathf.RoundToInt(Renderer.transform.position.y * 100f) * -1;
+        var sortingBack = Mathf.RoundToInt(MaskUp.position.y * 100f) * -1;
+        var sortingFront2 = Mathf.RoundToInt(MaskFront.position.y * 100f) * -1;
+        
+        Renderer.sortingOrder = sortingFront;
+        MaskRenderer.frontSortingOrder = sortingFront2;
+        MaskRenderer.backSortingOrder = sortingBack;
+    }
+
+    private void OnValidate()
+    {
+        Renderer = this.GetComponentInChildren<SpriteRenderer>();
+        MaskRenderer = this.GetComponentInChildren<SpriteMask>();
+    }
 }
--- Assets/API/Character.cs
+++ Assets/API/Character.cs
@@ -2,14 +2,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
+[ExecuteAlways]
 public class Character : MonoBehaviour
 {
-    public SpriteRenderer OwnRenderer;
+    public List<SpriteRenderer> OwnSorting;
     public Transform      OwnTransform;
 
     private void Update()
     {
-        OwnRenderer.sortingOrder = Mathf.RoundToInt(OwnTransform.position.y * 100f) * -1;
+        UpdateSprites();
+    }
+
+    private void UpdateSprites()
+    {
+        for (int c = 0; c < this.OwnSorting.Count; c++)
+        {
+            var sorting = Mathf.RoundToInt(this.OwnSorting[c].transform.position.y * 100f) * -1;
+            this.OwnSorting[c].sortingOrder = sorting;
+        }
     }
 }
