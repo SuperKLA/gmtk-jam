8ed8c280e358e46539dca9d8790779f923e5e1b2
Unknown <f.scholz85@outlook.de>
2020-07-11 19:15:53 +0200

    Terminal

---
+++ Assets/API/Terminal/TerminalBuildingDisplay.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TerminalBuildingDisplay : MonoBehaviour
+{
+    public TerminalBuilding terminalBuilding;
+
+    public Image buildingImage;
+    public TMP_Text buildingName;
+    public TMP_Text buildingState;
+
+    private void Start()
+    {
+        SetValues();
+    }
+
+    public void SetNewTerminalBuilding(TerminalBuilding newBuilding)
+    {
+        terminalBuilding = newBuilding;
+        SetValues();
+    }
+
+    private void SetValues()
+    {
+        buildingImage.sprite = terminalBuilding.image;
+        buildingName.text = terminalBuilding.name;
+        buildingState.faceColor = new Color32(255, 255, 255, 255);
+        if (terminalBuilding.state == BuildingState.ERROR)
+        {
+            StopAllCoroutines();
+            StartCoroutine(ERRORText());
+        }
+        else
+        {
+            StopAllCoroutines();
+            StartCoroutine(OKText());
+        }
+    }
+
+    public IEnumerator OKText()
+    {
+        //set the Text's text to blank
+        buildingState.text = ".";
+        yield return new WaitForSeconds(.5f);
+        buildingState.text = "..";
+        yield return new WaitForSeconds(.5f);
+        buildingState.faceColor = new Color32(0, 255, 0, 255);
+        buildingState.text = "OK";
+        yield return new WaitForSeconds(.5f);
+
+    }
+
+
+    public IEnumerator ERRORText()
+    {
+        buildingState.text = ".";
+        yield return new WaitForSeconds(.5f);
+        buildingState.text = "..";
+        yield return new WaitForSeconds(.5f);
+        buildingState.text = "";
+        buildingState.faceColor = new Color32(255, 0, 0, 255);
+        while (true)
+        {
+            buildingState.text = "";
+            yield return new WaitForSeconds(.5f);
+            buildingState.text = "ERROR";
+            yield return new WaitForSeconds(.5f);
+        }
+    }
+}
---
+++ Assets/API/Terminal/TerminalControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerminalControl : MonoBehaviour
+{
+
+    public TerminalBuildingDisplay tbp;
+
+    public TerminalBuilding Hospital;
+    public TerminalBuilding Police;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            tbp.SetNewTerminalBuilding(Hospital);
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            tbp.SetNewTerminalBuilding(Police);
+        }
+    }
+
+}
