ec4ccee15124d35d353bdd7ff06e8885bb322c9e
Konstantin Lapin <xKosta.1987@gmail.com>
2020-07-11 11:23:15 +0200

    WIP

--- Assets/API/Character.cs
+++ Assets/API/Character.cs
@@ -1,8 +1,15 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Character : MonoBehaviour
 {
-   
+    public SpriteRenderer OwnRenderer;
+    public Transform      OwnTransform;
+
+    private void Update()
+    {
+        OwnRenderer.sortingOrder = Mathf.RoundToInt(OwnTransform.position.y * 100f) * -1;
+    }
 }
