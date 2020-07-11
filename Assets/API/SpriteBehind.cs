42113f867333246a3f8b926899c2d1fa79f057c4
Konstantin Lapin <xKosta.1987@gmail.com>
2020-07-11 12:21:49 +0200

    WIP

---
+++ Assets/API/SpriteBehind.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+
+public class SpriteBehind : MonoBehaviour {
+    public Color color = Color.white;
+
+    public SpriteRenderer spriteRenderer;
+
+    void OnBecameVisible() {
+
+        UpdateOutline(true);
+    }
+
+    void OnBecameInvisible() {
+        UpdateOutline(false);
+    }
+
+    // void Update() {
+    //     UpdateOutline(true);
+    // }
+
+    void UpdateOutline(bool outline) {
+        MaterialPropertyBlock mpb = new MaterialPropertyBlock();
+        spriteRenderer.GetPropertyBlock(mpb);
+        mpb.SetColor("_Color", outline ? color : Color.clear);
+        spriteRenderer.SetPropertyBlock(mpb);
+    }
+}
