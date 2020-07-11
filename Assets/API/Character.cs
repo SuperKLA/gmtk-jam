e6299a9f412f273379d92c0ac23d516748ddc281
Konstantin Lapin <xKosta.1987@gmail.com>
2020-07-11 22:58:31 +0200

    Gebäude

--- Assets/API/Character.cs
+++ Assets/API/Character.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
 
+[ExecuteAlways]
 public class Character : MonoBehaviour
 {
     public List<SpriteRenderer> OwnSorting;
