e04edc79526c40a148057d2f6bd02d84351adecd
Konstantin Lapin <xKosta.1987@gmail.com>
2020-07-12 12:54:44 +0200

    FootSteps Sound

---
+++ Assets/API/CharacterAudio.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class CharacterAudio : MonoBehaviour
+{
+    public List<AudioSource> FootSteps;
+
+    private void OnValidate()
+    {
+        FootSteps = FootSteps ?? new List<AudioSource>();
+        FootSteps.Clear();
+        
+        FootSteps.AddRange(this.GetComponentsInChildren<AudioSource>(true));
+    }
+
+    public void PlayFootStep()
+    {
+        var i = Random.Range(0, this.FootSteps.Count);
+        this.FootSteps[i].Play();
+    }
+}
